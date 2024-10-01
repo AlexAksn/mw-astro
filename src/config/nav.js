49b5803f5627531440ed4b28eced82af661a0b3b
AlexanderA <AlexanderA@pressreader.com>
2024-10-01 12:36:29 -0700

    initial commit

---
+++ src/config/nav.js
@@ -0,0 +1,23 @@
+export const nav = [
+	{
+		title: 'Home',
+		slug: '/',
+	},
+	{
+		title: 'About Us',
+		slug: '/about',
+	},
+	{
+		title: 'Applications',
+		slug: '/applications',
+	},
+	{
+		title: 'Units',
+		slug: '/units',
+	},
+	{
+		title: 'Contact',
+		slug: '/contact',
+	}
+	
+];
---
+++ src/config/settings.js
@@ -0,0 +1,12 @@
+export default {
+	title: `Meadow Walk Housing Co-Operative | Richmond BC`,
+	description: `Meadow Walk Housing Co-Operative -
+ Comfortable apartments and townhomes for longterm rental. Centrally
+ located in the heart of Richmond, we are within walking distance of both an
+ elementary school and high school, parks, library, shopping and the Canada
+ Line.`,
+	url: `https://odyssey-theme.sapling.supply`, // No trailing slash!
+	name: `Meadow Walk`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	enableThemeSwitcher: true,
+	showPlug: false, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
+};
